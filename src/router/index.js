3e2ba7f0c7adf05b7290eea01bbb33619947fedc
fafurin <ffafurin@mail.ru>
2022-08-07 12:16:54 +0300

    added a front for the home page and product page

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'test',
-    //   component: () => import('../views/Test.vue')
-    // }
+    {
+      path: '/',
+      name: 'Main',
+      component: () => import('../views/Main/Index.vue')
+    },
+    {
+      path: '/products',
+      name: 'Products',
+      component: () => import('../views/Product/Index.vue')
+    },
   ]
 })
 
